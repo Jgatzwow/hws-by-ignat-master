602bbb5d82cef553c947cac929b6e42a53ec77ec
MSokol <mihasokol1993@gmail.com>
2022-10-16 13:59:32 +0300

    refactored hw4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -19,8 +19,9 @@ const SuperButton: React.FC<SuperButtonPropsType> = ({
 }) => {
   const finalClassName =
     s.button +
-    (disabled ? s.disabled : xType === "red" ? s.red : "") +
-    (className ? "" + className : ""); // задачка на смешивание классов
+    (disabled ? " " + s.disabled : xType === "red" ? " " + s.red : "") +
+    (className ? " " + className : "");
+  // задачка на смешивание классов
 
   return (
     <button

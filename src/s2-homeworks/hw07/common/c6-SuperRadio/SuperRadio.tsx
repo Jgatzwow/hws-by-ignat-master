22b4f8e89633f12f8d91c4087f5ed96ee80ef2c5
MSokol <mihasokol1993@gmail.com>
2022-10-30 16:31:04 +0300

    completed hw 7 without styles

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -50,7 +50,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
             className={finalRadioClassName}
             type={"radio"}
             name={o.id}
-            checked={o.id === value}
+            checked={o.id == value}
             value={value}
             onChange={onChangeCallback}
             {...restProps}
